0c5acd59b37bf3762a23a27ae29fd84b377ddce1
dogwith1eye <dogwith1eye@gmail.com>
2021-10-07 09:36:23 -0400

    Wait using a manualresetevent

--- Zio.cs
+++ Zio.cs
@@ -5,6 +5,7 @@ using static LaYumba.Functional.F;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Zio
 {
@@ -156,7 +157,7 @@ namespace Zio
         // method A => Unit
         sealed Unit Run(Func<A, Unit> callback) 
         {
-            var outer = true;
+            var mre = new ManualResetEvent(false);
             var stack = new Stack<dynamic>();
             dynamic currentZIO = this;
             var loop = true;
@@ -166,7 +167,6 @@ namespace Zio
                 if (stack.Count == 0)
                 {
                     loop = false;
-                    outer = false;
                     callback(value);
                 }
                 else
@@ -181,54 +181,51 @@ namespace Zio
             Unit Resume(dynamic nextZIO)
             {
                 currentZIO = nextZIO;
-                loop = true;
+                mre.Set();
                 return Unit();
             };
 
-            while (outer)
+            while (loop)
             {
-                while (loop)
+                switch (currentZIO.Label)
                 {
-                    switch (currentZIO.Label)
-                    {
-                        case "Succeed":
-                            Complete(currentZIO.Value);
-                            break;
-                        
-                        case "Effect":
-                            Complete(currentZIO.Thunk());
-                            break;
+                    case "Succeed":
+                        Complete(currentZIO.Value);
+                        break;
+                    
+                    case "Effect":
+                        Complete(currentZIO.Thunk());
+                        break;
 
-                        case "FlatMap":
-                            stack.Push(currentZIO.Cont);
-                            //Console.WriteLine("Push:" + stack.Count);
-                            currentZIO = currentZIO.Zio;
-                            break;
+                    case "FlatMap":
+                        stack.Push(currentZIO.Cont);
+                        //Console.WriteLine("Push:" + stack.Count);
+                        currentZIO = currentZIO.Zio;
+                        break;
 
-                        case "Async":
-                            if (stack.Count == 0)
-                            {
-                                loop = false;
-                                currentZIO.Register(callback);
-                            }
-                            else
-                            {
-                                loop = false;
-                                Func<dynamic, Unit> resume = (dyn) => Resume(dyn);
-                                currentZIO.Resume(resume);
-                            }
-                            break;
+                    case "Async":
+                        if (stack.Count == 0)
+                        {
+                            loop = false;
+                            currentZIO.Register(callback);
+                        }
+                        else
+                        {
+                            Func<dynamic, Unit> resume = (dyn) => Resume(dyn);
+                            currentZIO.Resume(resume);
+                            mre.WaitOne();
+                        }
+                        break;
 
-                        case "Fork":
-                            var fiber = currentZIO.CreateFiber();
-                            fiber.Start();
-                            Complete(fiber);
-                            break;
+                    case "Fork":
+                        var fiber = currentZIO.CreateFiber();
+                        fiber.Start();
+                        Complete(fiber);
+                        break;
 
-                        default: 
-                            throw new Exception("Zio case does not match");
-                    };
-                }
+                    default: 
+                        throw new Exception("Zio case does not match");
+                };
             }
             return Unit();
         }
